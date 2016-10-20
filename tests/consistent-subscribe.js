ff8cf8c13534763d2cace0d575ddfecf21c9cf5d
Gvozd <gvozdev.viktor@gmail.com>
2016-10-20 20:16:55 +0300

    feature: define options configuration

--- tests/consistent-subscribe.js
+++ tests/consistent-subscribe.js
@@ -16,6 +16,16 @@ ruleTester.run('consistent-subscribe', rule, {
             $('body').on('click', onClick);
             $('body').off('click', onClick);
         `,
+        `
+            foo.start();
+        `,
+        {
+            code: `
+                foo.start();
+                foo.stop();
+            `,
+            options: [[{open: 'start', close: 'stop'}]]
+        },
         // fixed crashes
         `
             (function() {})();
@@ -33,7 +43,14 @@ ruleTester.run('consistent-subscribe', rule, {
                 function onClick() {}
                 document.addEventListener('click', onClick);
             `,
-            errors: [{message: 'unpaired addEventListener', type: 'CallExpression'}]
+            errors: [{message: 'unpaired "addEventListener"', type: 'CallExpression'}]
+        },
+        {
+            code: `
+                foo.start();
+            `,
+            options: [[{open: 'start', close: 'stop'}]],
+            errors: [{message: 'unpaired "start"', type: 'CallExpression'}]
         }
     ]
 });
